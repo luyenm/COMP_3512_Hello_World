7dfb8ae63078c174587bdf177dc4ac48eba34f58
Arlios <luyen_mai@Live.com>
2018-01-18 15:47:24 -0800

    Final changes to GCD

--- Hello World/Hello World/lab1.cpp
+++ Hello World/Hello World/lab1.cpp
@@ -9,30 +9,10 @@
 int gcd(const int a, const int b)
 {
 	// Replace this line with your code (have fun!).
-	int gcf{ 0 };
-	int temp1 = a;
-	int temp2 = b;
-	while (temp1 % 2 == 0 && temp2 % 2 == 0) {
-		temp1 /= 2;
-		temp2 /= 2;
-		gcf += 1;
+	if (b == 0) {
+		return a;
 	}
-	while (temp1 != b) {
-		if (temp1 % 2 == 0) {
-			temp1 /= 2;
-		} else if (temp2 % 2 == 0) {
-			temp2 /= 2;
-		}
-		else if (temp1 > temp2) {
-			temp1 = (temp1 - temp2) / 2;
-		}
-		else {
-			temp2 = (temp2 - temp1) / 2;
-		}
-
-	}
-	gcf = temp1;
-	return gcf;
+	return gcd(b, a % b);
 }
 
 // Fibonacci
@@ -41,7 +21,7 @@ int gcd(const int a, const int b)
 // RETURN: the nth positive integer in the Fibonacci sequence.
 long long fibonacci(const int n)
 {
-	if (n <= 1) {
+	if (n == 1 || n == 2) {
 		return n;
 	}
 	else {
